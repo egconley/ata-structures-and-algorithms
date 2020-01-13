4de3958468a0209176f8b0fa8b060813f581ce0c
egconley <31393715+egconley@users.noreply.github.com>
2020-01-13 11:26:26 -0800

    finished LinkedList and Node classes

---
+++ code401challenges/src/main/java/code401challenges/linkedlist/Node.java
@@ -0,0 +1,41 @@
+package code401challenges.linkedlist;
+
+// source: https://www.csie.ntu.edu.tw/~cyy/courses/oop/13summer/chap15java5th.pdf
+
+public class Node {
+    private String item;
+    private int count;
+    private Node link;
+
+    public Node() {
+        link = null;
+        item = null;
+        count = 0;
+    }
+
+    public Node(String newItem, int newCount, Node linkValue) {
+        setData(newItem, newCount);
+        link = linkValue;
+    }
+
+    public void setData(String newItem, int newCount) {
+        item = newItem;
+        count = newCount;
+    }
+
+    public void setLink(Node newLink) {
+        link = newLink;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Node getLink() {
+        return link;
+    }
+}
---
+++ code401challenges/src/test/java/code401challenges/linkedlist/LinkedListTest.java
@@ -0,0 +1,12 @@
+package code401challenges.linkedlist;
+
+import code401challenges.ArrayShift;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedListTest {
+
+
+}
