937adef100dfcd0da8940b4078faea9d26ec333b
egconley <31393715+egconley@users.noreply.github.com>
2020-05-01 11:45:06 -0700

    pow

---
+++ code401challenges/src/test/java/code401challenges/powTest.java
@@ -0,0 +1,29 @@
+package code401challenges;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class powTest {
+
+    pow test = new pow();
+    @Test
+    public void powZero() {
+        assertEquals(1, test.pow(5, 0), 0.001);
+    }
+
+    @Test
+    public void powOne() {
+        assertEquals(5, test.pow(5, 1), 0.001);
+    }
+
+    @Test
+    public void powNegative() {
+        assertEquals(0.5, test.pow(2, -1), 0.001);
+    }
+
+    @Test
+    public void pow() {
+        assertEquals(243, test.pow(3, 5), 0.001);
+    }
+}
