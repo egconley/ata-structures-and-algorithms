1fb4a2cd93d3d115f4aefdb64c54b083bc04199b
egconley <31393715+egconley@users.noreply.github.com>
2020-05-08 17:15:27 -0700

    makeAnagram

---
+++ code401challenges/src/test/java/code401challenges/makeAnagramTest.java
@@ -0,0 +1,34 @@
+package code401challenges;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class makeAnagramTest {
+
+    makeAnagram test = new makeAnagram();
+
+    @Test
+    public void makeAnagram1() {
+        String a = "aab";
+        String b = "ab";
+        int actual = test.makeAnagram(a, b);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void makeAnagram2() {
+        String a = "aab";
+        String b = "abb";
+        int actual = test.makeAnagram(a, b);
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void makeAnagram3() {
+        String a = "aabc";
+        String b = "abbe";
+        int actual = test.makeAnagram(a, b);
+        assertEquals(4, actual);
+    }
+}
