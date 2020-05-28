0ce6c6923ffe6aeb510ee68425408e89d2d29865
egconley <31393715+egconley@users.noreply.github.com>
2020-05-28 14:29:39 -0700

    permutations

---
+++ code401challenges/src/test/java/code401challenges/PermutationsTest.java
@@ -0,0 +1,19 @@
+package code401challenges;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PermutationsTest {
+
+    Permutations test = new Permutations();
+    
+    @Test
+    public void permutation() {
+        assertTrue(test.permutation("CAT", "TCA"));
+        assertFalse(test.permutation("DOG", "CAT"));
+        assertFalse(test.permutation("n", "nn"));
+        assertTrue(test.permutation(null, null));
+        assertFalse(test.permutation("cat", "CAT"));
+    }
+}
